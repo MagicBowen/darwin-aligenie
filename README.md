0b137a55eb6de498ebf18f95b9a72e4651b3dce3
magicbowen <e.bowen.wang@icloud.com>
2018-11-29 14:29:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A KOA server kickoff framework.
---
+++ controllers/index.js
@@ -0,0 +1,28 @@
+var index = async (ctx, next) => {
+    await ctx.render('index.html');
+};
+
+var postQuery = async (ctx, next) => {
+    const request = ctx.request.body
+    const query = request.utterance
+    console.log(JSON.stringify(request))
+    ctx.response.type = "application/json";
+    ctx.response.status = 200;
+    ctx.response.body = {
+    "returnCode": "0",
+    "returnErrorSolution": "",
+    "returnMessage": "",
+    "returnValue": {
+        "reply": "你好，欢迎进入课程表，功能正在开发中，请稍后...",
+        "resultType": "RESULT",
+        "properties": {},
+        "executeCode": "SUCCESS",
+        "msgInfo": ""
+    }
+}  
+};
+
+module.exports = {
+    'GET /': index,
+    'POST /': postQuery
+};

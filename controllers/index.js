d4f63765c69ef74dd69a7dd724964714758bfef5
magicbowen <e.bowen.wang@icloud.com>
2018-11-29 16:27:27 +0800

    modify code

--- controllers/index.js
+++ controllers/index.js
@@ -1,31 +1,85 @@
-var index = async (ctx, next) => {
+const Chatbot = require('darwin-sdk').Chatbot
+const Query = require('darwin-sdk').Query
+const OpenSkillEvent = require('darwin-sdk').OpenSkillEvent
+const config = require('./config')
+
+const index = async (ctx, next) => {
     await ctx.render('index.html');
 };
 
-var postQuery = async (ctx, next) => {
-    const request = ctx.request.body
-    const query = request.utterance
-    console.log(JSON.stringify(request))
-    ctx.response.type = "application/json";
-    ctx.response.status = 200;
-    ctx.response.body = {
-    "returnCode": "0",
-    "returnErrorSolution": "",
-    "returnMessage": "",
-    "returnValue": {
-        "reply": "你好，欢迎进入课程表，功能正在开发中，请稍后...",
-        "resultType": "ASK_INF",
-        "askedInfos":[
-            {
-                "parameterName" : "any",
-                "intentId" : 28239
-            }
-        ],
-        "properties": {},
-        "executeCode": "SUCCESS",
-        "msgInfo": ""
+const getAgentBySkillName = (skillName) => {
+    return config.agents[skillName]
+}
+
+const getContinueResponse = (intentId, response) => {
+    return {
+        "returnCode": "0",
+        "returnErrorSolution": "",
+        "returnMessage": "",
+        "returnValue": {
+            "reply": response.getReply(),
+            "resultType": "ASK_INF",
+            "askedInfos":[
+                {
+                    "parameterName" : "any",
+                    "intentId" : intentId
+                }
+            ],
+            "properties": {},
+            "executeCode": "SUCCESS",
+            "msgInfo": ""
+        }
+    } 
+}
+
+const getFinalResponse = (code, response) => {
+    return {
+        "returnCode": "0",
+        "returnErrorSolution": "",
+        "returnMessage": "",
+        "returnValue": {
+            "reply": response.getReply(),
+            "resultType": "RESULT",
+            "properties": {},
+            "executeCode": code,
+            "msgInfo": ""
+        }
     }
-}  
+}
+
+const getFailResponse = () => {
+    return getFinalResponse("PARAMS_ERROR", "请求发生错误，请稍后重试，谢谢！")
+}
+
+const isOpenApp = (request) => {
+    return request.slotEntities.length === 0
+}
+
+const buildQueryBy = (request) => {
+    const sessionId = request.sessionId
+    const tocken = request.token
+    const query = isOpenApp(request) ? new OpenSkillEvent(sessionId) : new Query(sessionId, request.utterance)
+    if (tocken) query.setAccessTocken(tocken)
+    return query
+}
+
+const getChatbotResponse = (request) => {
+    const request = ctx.request.body
+    const agent = getAgentBySkillName(request.skillName)
+
+    const chatbot = new Chatbot(config.chatbot_url, agent, config.source)
+    const query = buildQueryBy(request)
+    const rsp = chatbot.dispose(query)
+    return rsp.hasInstructOfQuit() ? getFinalResponse('SUCCESS', rsp) : getContinueResponse(request.intentId, rsp)
+}
+
+const postQuery = async (ctx, next) => {
+    const request = ctx.request.body
+    const agent = getAgentBySkillName(request.skillName)
+    const response = agent ? getChatbotResponse(request) : getFailResponse()
+    ctx.response.type = "application/json"
+    ctx.response.status = 200
+    ctx.response.body = response
 };
 
 module.exports = {

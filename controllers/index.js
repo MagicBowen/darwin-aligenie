6c889ad4b66a070b5df06472bf3b832292c64dbd
magicbowen <e.bowen.wang@icloud.com>
2018-11-30 11:17:37 +0800

    add actions

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const Query = require('darwin-sdk').Query
 const OpenSkillEvent = require('darwin-sdk').OpenSkillEvent
 const Response = require('darwin-sdk').Response
 const config = require('../config')
+const logger = require('../utils/logger')('bot')
 
 const index = async (ctx, next) => {
     await ctx.render('index.html');
@@ -12,40 +13,53 @@ const getAgentBySkillName = (skillName) => {
     return config.agents[skillName]
 }
 
+const mapUrlToResourceId = (url) => {
+    return ""
+}
+
 const getContinueResponse = (intentId, response) => {
     return {
-        "returnCode": "0",
-        "returnErrorSolution": "",
-        "returnMessage": "",
-        "returnValue": {
-            "reply": response.getReply(),
-            "resultType": "ASK_INF",
-            "askedInfos":[
+        returnCode: "0",
+        returnErrorSolution: "",
+        returnMessage: "",
+        returnValue: {
+            reply: response.getReply(),
+            resultType: "ASK_INF",
+            askedInfos:[
                 {
-                    "parameterName" : "any",
-                    "intentId" : intentId
+                    parameterName : "any",
+                    intentId : intentId
                 }
             ],
-            "properties": {},
-            "executeCode": "SUCCESS",
-            "msgInfo": ""
+            properties: {},
+            executeCode: "SUCCESS",
+            msgInfo: ""
         }
     } 
 }
 
 const getFinalResponse = (code, response) => {
-    return {
-        "returnCode": "0",
-        "returnErrorSolution": "",
-        "returnMessage": "",
-        "returnValue": {
-            "reply": response.getReply(),
-            "resultType": "RESULT",
-            "properties": {},
-            "executeCode": code,
-            "msgInfo": ""
+    const result = {
+        returnCode: "0",
+        returnErrorSolution: "",
+        returnMessage: "",
+        returnValue: {
+            reply: response.getReply(),
+            resultType: "RESULT",
+            properties: {},
+            executeCode: code,
+            msgInfo: ""
         }
     }
+    const actions = []
+    for (let instruct of response.getInstructs()) {
+        if (instruct.type === 'play-audio') {
+            actions.push({ name: "audioPlayGenieSource", properties: {audioGenieId : mapUrlToResourceId(instruct.url)}})
+        }
+    }
+
+    if (actions.length > 0) result.returnValue.actions = actions
+    return result
 }
 
 const getFailResponse = () => {
@@ -75,8 +89,10 @@ const getChatbotResponse = async (request) => {
 
 const postQuery = async (ctx, next) => {
     const request = ctx.request.body
+    logger.debug(`receive : ${JSON.stringify(request)}`)
     const agent = getAgentBySkillName(request.skillName)
     const response = agent ? await getChatbotResponse(request) : getFailResponse()
+    logger.debug(`reply : ${JSON.stringify(response)}`)
     ctx.response.type = "application/json"
     ctx.response.status = 200
     ctx.response.body = response
